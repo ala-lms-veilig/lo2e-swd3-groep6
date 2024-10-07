2d6e9531591c01c3149902a0b6fbe4f427443a57
Alexandros <chanizmpekidisaleko@gmail.com>
2024-10-07 10:19:51 +0200

    login en login js

---
+++ js/login.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contentDiv = document.getElementById('content');
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+    if (isLoggedIn === 'true') {
+        contentDiv.innerHTML = `
+            <h2>Welkom!</h2>
+            <p>Je bent ingelogd.</p>
+            <button id="logoutButton">Uitloggen</button>
+        `;
+        
+        document.getElementById('logoutButton').addEventListener('click', function() {
+            localStorage.removeItem('isLoggedIn');
+            location.reload(); 
+        });
+    } else {
+        contentDiv.innerHTML = `
+            <article class="formlogin"> 
+                <form id="loginForm">
+                    <h2>Inloggen</h2>
+                    Gebruikersnaam:<br>
+                    <input type='text' class="logininput" name='gebruikersnaam' id="gebruikersnaam"><br><br>
+                    Wachtwoord:<br>
+                    <input type='password' class="logininput" name='wachtwoord' id="wachtwoord"><br><br>
+                    <input type='submit' class="loginsubmit" value='Inloggen'>
+                </form>
+            </article>
+        `;
+        
+        document.getElementById('loginForm').addEventListener('submit', function(event) {
+            event.preventDefault(); 
+            
+            const gebruikersnaam = document.getElementById('gebruikersnaam').value;
+            const wachtwoord = document.getElementById('wachtwoord').value;
+
+            if (gebruikersnaam === 'admin' && wachtwoord === '1234') {
+                localStorage.setItem('isLoggedIn', 'true');
+                location.reload(); 
+            } else {
+                alert('Onjuiste gebruikersnaam of wachtwoord!');
+            }
+        });
+    }
+});
