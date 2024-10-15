8b4cd0b856f1143b077f007b418cc205a178b145
Alexandros <chanizmpekidisaleko@gmail.com>
2024-10-15 11:23:09 +0200

    yes

--- js/klachten.js
+++ js/klachten.js
@@ -1,3 +1,21 @@
-function submitKnop(event) {
-     
+function ShowKlachten() {
+    
+}
+
+async function submitKnop(event) {
+    const newKlacht = document.getElementById("klachtButton").value;
+
+    const response = fetch(klachten.json, {
+        method: "POST",
+        body: JSON.stringify({
+            title: newKlacht,
+        }),
+        headers: {
+            'Content-type': 'applicataion/json; charset=UTF-8',
+        },
+     });
+
+     const post = await response.json();
+
+     const klachtInvul = document.getElementById()
 }
