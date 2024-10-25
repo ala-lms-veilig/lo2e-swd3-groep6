c86c4459637d0a3311e2afe422eca12bfe7c3e52
loldnc <Zaoukim6@gmail.com>
2024-10-25 12:00:27 +0200

    logout button

--- js/home.js
+++ js/home.js
@@ -34,4 +34,17 @@ async function loadData() {
     
     
     loadData();
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const isLoggedIn = localStorage.getItem('isLoggedIn');
     
+
+        if (isLoggedIn !== 'true') {
+            window.location.href = 'login.html';
+        }
+    
+        document.getElementById('logoutButton').addEventListener('click', function() {
+            localStorage.removeItem('isLoggedIn');
+            window.location.href = 'login.html'; 
+        });
+    });
