e9b01d35107e3b69febe6c0248ad37cec69ea84f
Alexandros <chanizmpekidisaleko@gmail.com>
2024-10-25 13:59:48 +0200

    Login wijzigen jsons gelinked

--- js/login.js
+++ js/login.js
@@ -1,22 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginSection = document.getElementById('loginSection');
     const isLoggedIn = localStorage.getItem('isLoggedIn');
-
     if (isLoggedIn === 'true') {
-        window.location.href = 'home.html';
+        window.location.href = 'home.html'; 
     } else {
-        loginSection.style.display = 'block';
-
-       
+        loginSection.style.display = 'block'; 
         const usersData = JSON.parse(document.getElementById('userData').textContent);
 
+   
         document.getElementById('loginForm').addEventListener('submit', function(event) {
             event.preventDefault();
 
             const gebruikersnaam = document.getElementById('gebruikersnaam').value;
             const wachtwoord = document.getElementById('wachtwoord').value;
 
-       
+      
             const user = usersData.gebruikers.find(user => 
                 user.gebruikersnaam === gebruikersnaam && user.wachtwoord === wachtwoord
             );
